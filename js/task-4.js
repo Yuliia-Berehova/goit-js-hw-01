42626c75473d1b8a235695e68e08b098664db5ee
yuliia berehova <berehova.iuliia@gmail.com>
2020-07-16 02:15:16 +0300

    task-4 correction added

--- js/task-4.js
+++ js/task-4.js
@@ -3,19 +3,22 @@ const pricePerDroid = 3000;
 let totalPrice;
 
 const droidsAmount = prompt('Введите количество дроидов');
+
 if (droidsAmount === null) {
   console.log('Отменено пользователем!');
-} else if (isNaN(droidsAmount)) {
+} else if (Number.isNaN(Number(droidsAmount))) {
   console.log('Введите число!');
 } else {
   totalPrice = pricePerDroid * Number(droidsAmount);
-  console.log(totalPrice);
 }
-if (totalPrice > credits) {
-  console.log('Недостаточно средств на счету!');
-} else {
-  credits = credits - totalPrice;
-  console.log(
-    `Вы купили ${droidsAmount} дроидов, на счету осталось ${credits} кредитов`,
-  );
+
+if (droidsAmount !== null && !Number.isNaN(Number(droidsAmount))) {
+  if (totalPrice > credits) {
+    console.log('Недостаточно средств на счету!');
+  } else {
+    credits = credits - totalPrice;
+    console.log(
+      `Вы купили ${droidsAmount} дроидов, на счету осталось ${credits} кредитов`,
+    );
+  }
 }
